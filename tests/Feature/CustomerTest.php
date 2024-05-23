b78dda5d87488613fb07d5360c89ea965cb73f75
alirezaporthos <alirezaportous@gmail.com>
2024-05-23 13:40:56 +0330

    Add tests for testing crud methods

---
+++ tests/Feature/CustomerTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CustomerTest extends TestCase
+{
+    public function test_can_get_customer_by_id()
+    {
+        $customer = \App\Models\Customer::factory()->create();
+
+        $response = $this->get("/api/customers/{$customer->id}");
+
+        $response->assertStatus(200);
+
+        $response->assertJsonFragment([
+            'id' => $customer->id,
+            'first_name' => $customer->first_name,
+            'last_name' => $customer->last_name,
+            'date_of_birth' => $customer->date_of_birth,
+            'phone_number' => $customer->phone_number,
+            'email' => $customer->email,
+            'bank_account_number' => $customer->bank_account_number,
+        ]);
+    }
+
+    public function test_can_create_customer()
+    {
+
+        $data = [
+            'first_name' => 'Alireza',
+            'last_name' => 'Porthos',
+            'date_of_birth' => now(),
+            'phone_number' => 1234567890,
+            'email' => 'alireza@example.com',
+            'bank_account_number' => '1234567890123456',
+        ];
+
+        $response = $this->post('/api/customers', $data);
+
+        $response->assertStatus(201);
+        $response->assertJsonFragment($data);
+    }
+
+    public function test_can_update_customer()
+    {
+        $customer = \App\Models\Customer::factory()->create();
+
+        $updatedData = [
+            'first_name' => 'Porthos',
+            'last_name' => 'Alireza',
+        ];
+
+        $response = $this->put("/api/customers/{$customer->id}", $updatedData);
+
+        $response->assertStatus(200);
+        $response->assertJsonFragment($updatedData);
+    }
+
+    public function test_can_delete_customer()
+    {
+        $customer = \App\Models\Customer::factory()->create();
+
+        $response = $this->delete("/api/customers/{$customer->id}");
+
+        $response->assertStatus(204);
+    }
+}
